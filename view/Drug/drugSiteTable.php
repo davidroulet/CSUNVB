c2443fb71d26908fbd31207788bb743f9cbd4f51
ROULET David <David.ROULET@cpnv.ch>
2020-02-04 16:45:52 +0100

    Dug Update

--- view/Drug/drugSiteTable.php
+++ view/Drug/drugSiteTable.php
@@ -4,13 +4,9 @@ $title = "CSU-NVB - Stupéfiants";
 ?>
 <div class="row m-2">
     <h1>Stupéfiants</h1>
-    <form>
-        <button> Pyerne
-        </button>
-
-    </form>
 </div>
 <?php
 $content = ob_get_clean();
 require "view/gabarit.php";
+
 ?>
