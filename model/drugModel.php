332296c14dcaa844269e21ec953cf355b4471fc7
XCarrel <Xavier.Carrel@cpnv.ch>
2020-02-04 16:28:11 +0100

    Unit test example

--- model/drugModel.php
+++ model/drugModel.php
@@ -10,9 +10,10 @@
  * Des points seront également retirés au groupe qui osera laisser une des fonctions de ce fichier telle quelle
  * sans l'adapter au niveau de son nom et de son code pour qu'elle dise plus précisément de quelles données elle traite
  */
-function readDrugItems()
+function getDrugItems()
 {
-    return json_decode(file_get_contents("model/dataStorage/items.json"),true);
+    $data = ['tableau','de','chaînes','de','caractères'];
+    return $data;
 }
 
 /**
