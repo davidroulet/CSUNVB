57927f814402d7718d117d71eb780b5d38de334a
ALHELO Marwan <Marwan.ALHELO@cpnv.ch>
2020-02-06 12:15:29 +0100

    wip todo list

--- view/todo/todoListHome.php
+++ view/todo/todoListHome.php
@@ -5,8 +5,9 @@ $title = "CSU-NVB - Tâches hebdomadaires";
 <div class="row m-2">
     <h1>Tâches hebdomadaires</h1>
     <table class="table table-bordered">
-        <thead>
+        <thead class="thead-light">
         <tr>
+            <th rowspan="2"></th>
             <th>Lundi</th>
             <th>Mardi</th>
             <th>Mercredi</th>
@@ -15,24 +16,98 @@ $title = "CSU-NVB - Tâches hebdomadaires";
             <th>Samedi</th>
             <th>Dimanche</th>
 
+        </tr>
+        <tr>
+
+            <th>date</th>
+            <th>...</th>
+            <th>...</th>
+            <th>...</th>
+            <th>...</th>
+            <th>...</th>
+            <th>...</th>
 
         </tr>
         </thead>
         <tbody>
-        <?php
 
-        for ($i=1; $i > 100; $i++) {
-            echo "<tr>";
-            echo "<td>" .$i."</td>";
-            echo "<td>" . $i. "</td>";
-            echo "<td>" . $i. "</td>";
-            echo "</tr>";
-            var_dump($i);
-        }
-        ?>
+
+        <tr >
+            <td rowspan="6"><span class="horizontal"> jour</span></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+        </tr>
+
+
         </tbody>
-
     </table>
+
+
+        <?php
+        /*
+        for ($i=1; $i <10; $i++) {
+            echo "<tr>";
+            echo "<td>" .$i." : Do something"."</td>";
+
+            echo "<td>" . $i." : Do something". "</td>";
+            echo "<td>" . $i." : Do something" ."</td>";
+            echo "<td>" . $i. " : Do something"."</td>";
+            echo "<td>" . $i." : Do something" ."</td>";
+            echo "<td>" . $i." : Do something" ."</td>";
+            echo "<td>" . $i." : Do something" ."</td>";
+            echo "</tr>";
+
+        }
+        */
+        ?>
+
+
+
 </div>
 <?php
 $content = ob_get_clean();
