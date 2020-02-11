d63b83d44696828d4377830094b203ffae6e5245
AltinZili <47849648+AltinZili@users.noreply.github.com>
2020-02-11 16:54:40 +0100

    un peu de travail sur le uniTestTodo

--- model/unitTests/todoTest.php
+++ model/unitTests/todoTest.php
@@ -1,8 +1,42 @@
 <?php
 
+require 'todoListModel.php';
 
+$todoItems = readTodoListItems();
 
 ?>
+
+
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Modèle</th>
+            <th>Marque</th>
+            <th>Image</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($todoItems as $todoItem) { ?>
+        <tr>
+            <td><?= $todoItem['id'] ?></td>
+            <td><?= $todoItem['date'] ?></td>
+            <td><?=$todoItem["base"]?>" </td>
+            <td><?= $todoItem['nightjob'] ?></td>
+            <td><?= $todoItem['description'] ?></td>
+            <td><?=$todoItem["type"]?>" </td>
+            <td><?=$todoItem["value"]?>" </td>
+
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
+
+<?php
+var_dump();
+
+?>
+
+
 
 
 
