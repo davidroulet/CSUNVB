62444dd652a75980cb62fd627eff7c501afb1014
Fabien Masson <47849849+fabien-masson@users.noreply.github.com>
2020-02-11 14:52:52 +0100

    wip

--- view/Drug/drugSiteTable.php
+++ view/Drug/drugSiteTable.php
@@ -3,79 +3,46 @@ ob_start();
 $title = "CSU-NVB - Stupéfiants";
 $site = "Payerne";
 ?>
-<div class="row m-2">
-    <h1>Stupéfiants</h1>
-</div>
-
-
-<div class="week">
-    <div class="day">
-        <div class="dayheader">""</div>
-        <div class="dayheader">Morphine N°</div>
+    <div class="row m-2">
+        <h1>Stupéfiants</h1>
     </div>
 
-    <div class="day">
-        <div class="dayheader">Lundi</div>
-        <div class="week">
-            <div class="day">
-                <div class="dayheader">75</div>
-                <div class="hour">75</div>
-                <div class="hour">76</div>
-                <div class="hour">77</div>
-            </div>
-            <div class="day">
-                <div class="dayheader">76</div>
-                <div class="hour">75</div>
-                <div class="hour">76</div>
-                <div class="hour">77</div>
-            </div>
-            <div class="day">
-                <div class="dayheader">77</div>
-                <div class="hour">75</div>
-                <div class="hour">76</div>
-                <div class="hour">77</div>
+    <div class="week">
+        <div class="day">
+            <div class="dayheader">Jours de la semaine</div>
+            <div class="week2">
+                <div class="day">
+                    <div class="dayheader">N° Ambulance</div>
+                    <div class="hour">Pharmacie</div>
+                    <div class="hour2">Morphine N°</div>
+                </div>
             </div>
         </div>
+        <?php
+        $jours = array("Lundi", "Mardi", "Mercredi", "Jeudi", "vendredi", "samedi", "dimanche");
+        foreach ($jours as $jour) { ?>
+            <div class="day">
+                <div class="dayheader"> <?= $jour ?> </div>
+                <div class="week2">
+                    <div class="day">
+                        <div class="dayheader">75</div>
+                        <div class="hour">X</div>
+                        <div class="hour2">X</div>
+                    </div>
+                    <div class="day">
+                        <div class="dayheader">76</div>
+                        <div class="hour">X</div>
+                        <div class="hour2">X</div>
+                    </div>
+                    <div class="day">
+                        <div class="dayheader">77</div>
+                        <div class="hour">X</div>
+                        <div class="hour2">X</div>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
     </div>
-    <div class="day">
-        <div class="dayheader">Mardi</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-    </div>
-    <div class="day">
-        <div class="dayheader">Mercredi</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-    </div>
-    <div class="day">
-        <div class="dayheader">Jeudi</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-    </div>
-    <div class="day">
-        <div class="dayheader">Vendredi</div>
-        <div class="hour">Do something</div>
-    </div>
-    <div class="day">
-        <div class="dayheader">Samedi</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-    </div>
-    <div class="day">
-        <div class="dayheader">Dimanche</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-        <div class="hour">Do something</div>
-    </div>
-</div>
-
-
 <?php
 $content = ob_get_clean();
 require "view/gabarit.php";
