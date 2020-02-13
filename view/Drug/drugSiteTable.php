9c4c811981c02fe9db93eeed1b6c3ed5ec1e2005
Fabien Masson <47849849+fabien-masson@users.noreply.github.com>
2020-02-13 11:47:50 +0100

    table

--- view/Drug/drugSiteTable.php
+++ view/Drug/drugSiteTable.php
@@ -6,58 +6,78 @@ $site = "Payerne";
     <div class="row m-2">
         <h1>Stupéfiants</h1>
     </div>
+<?php
+$site="Payerne";
+$semaine="50";
+$date="10.02.20"; ?>
+<h2>Site de <?= $site ?> , Semaine N° <?= $semaine ?></h2>
 
-    <div class="week">
-        <div class="day">
-            <div class="dayheader">Jours</div>
-            <div class="week">
-                <div class="day">
-                    <div class="dayheader">N° Ambulance</div>
-                    <div class="hour">Pharmacie</div>
-                    <div class="hour2">Morphine N°</div>
-                    <div class="hour2">Fentanyl N°</div>
-                    <div class="hour2">TEMESTA</div>
-                </div>
-            </div>
-        </div>
-        <?php
-        $jours = array("Lundi", "Mardi", "Mercredi", "Jeudi", "vendredi", "samedi", "dimanche");
-        foreach ($jours as $jour) { ?>
-            <div class="day">
-                <div class="dayheader"> <?= $jour ?> </div>
-                <div class="week">
-                    <div class="day">
-                        <div class="dayheader">Ph</div>
-                        <div class="hour">X</div>
-                        <div class="hour2">X</div>
-                        <div class="hour">X</div>
-                        <div class="hour2">X</div>
-                    </div>
-                    <div class="day">
-                        <div class="dayheader">75</div>
-                        <div class="hour">X</div>
-                        <div class="hour2">X</div>
-                        <div class="hour">X</div>
-                        <div class="hour2">X</div>
-                    </div>
-                    <div class="day">
-                        <div class="dayheader">76</div>
-                        <div class="hour">X</div>
-                        <div class="hour2">X</div>
-                        <div class="hour">X</div>
-                        <div class="hour2">X</div>
-                    </div>
-                    <div class="day">
-                        <div class="dayheader">77</div>
-                        <div class="hour">X</div>
-                        <div class="hour2">X</div>
-                        <div class="hour">X</div>
-                        <div class="hour2">X</div>
-                    </div>
-                </div>
-            </div>
-        <?php } ?>
-    </div>
+<?php
+$jours = array("Lundi", "Mardi", "Mercredi", "Jeudi", "vendredi", "samedi", "dimanche");
+
+foreach ($jours as $jour) { ?>
+<table border="1" class="table table-dark">
+    <tr>
+        <td colspan="5"><?= $jour." ".$date?></td>
+
+    </tr>
+    <tr>
+        <td></td>
+        <td>Pharmacie</td>
+        <td>75</td>
+        <td>76</td>
+        <td>77</td>
+    </tr>
+    <tr>
+        <td>Morphine N°</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>x</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Fentanyl N°</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>x</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Temestra</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>x</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>Signature</td>
+        <td colspan="4"></td>
+
+    </tr>
+</table>
+    <?php } ?>
+
 <?php
 $content = ob_get_clean();
 require "view/gabarit.php";
