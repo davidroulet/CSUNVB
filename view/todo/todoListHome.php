735afdc1b030b5b5c439965d52d7389a4a37420b
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2020-02-25 15:40:06 +0100

    Revert "Merge branch 'master' of https://github.com/davidroulet/CSUNVB"
    This reverts commit 2894075bac4cb3474caf68f20c5902b53fa14b3a, reversing
    changes made to 9971c9db349b95947027185737d7505a2ff27f9b.

--- view/todo/todoListHome.php
+++ view/todo/todoListHome.php
@@ -5,145 +5,196 @@ $title = "CSU-NVB - Tâches hebdomadaires";
 
 
 <h1 class="center"><?=$title?></h1>
-<div class="container ">
-    <div class="week">
-        <div class="horizontal"> <span style="font-weight: bold"> Journée</span> </div>
-        <div class="day col-sm">
+<div>
+    <div class="week col-2">
+        <div class="horizontal"> <span style="font-weight: bold">  journée</span> </div>
+        <div class="day">
+
             <div class="dayheader">Lundi</div>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Fax 144  Transmission</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check    Ambulance et  Communication</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check bibliothèque</div></a>
-            <a href="?action=Edittodo" class="over" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Nettoyage centrale et garage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Tâches spécifique</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Formation</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Remise locaux ambulances</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Fax 144  Transmission</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check    Ambulance et  Communication</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check bibliothèque</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Nettoyage centrale et garage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Tâches spécifique</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Formation</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Remise locaux ambulances</div></a>
 
         </div>
-        <div class="day col-sm">
+        <div class="day">
             <div class="dayheader">Mardi</div>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Fax 144  Transmission</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check    Ambulance et  Communication</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check bibliothèque</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Changer Bac chariot de nettoyage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Nettoyage centrale et garage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Tâches spécifique</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Formation</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Remise locaux ambulances</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Fax 144  Transmission</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check    Ambulance et  Communication</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check bibliothèque</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Nettoyage centrale et garage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Tâches spécifique</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Formation</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Remise locaux ambulances</div></a>
         </div>
-        <div class="day col-sm">
+        <div class="day">
             <div class="dayheader">Mercredi</div>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Fax 144  Transmission</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check    Ambulance et  Communication</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check bibliothèque</div></a>
-            <a href="?action=Edittodo" class="over" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Nettoyage centrale et garage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Tâches spécifique</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Formation</div></a>
-            <a href="?action=Edittodo" class="over" > <div class="hour">Remise locaux ambulances</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Fax 144  Transmission</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check    Ambulance et  Communication</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check bibliothèque</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Nettoyage centrale et garage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Tâches spécifique</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Formation</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Remise locaux ambulances</div></a>
         </div>
-        <div class="day col-sm">
+        <div class="day">
             <div class="dayheader">Jeudi</div>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Fax 144  Transmission</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check    Ambulance et  Communication</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check bibliothèque</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Changer Bac chariot de nettoyage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Nettoyage centrale et garage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Tâches spécifique</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Formation</div></a>
-            <a href="?action=Edittodo" class="over" > <div class="hour">Remise locaux ambulances</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Fax 144  Transmission</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check    Ambulance et  Communication</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check bibliothèque</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Nettoyage centrale et garage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Tâches spécifique</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Formation</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Remise locaux ambulances</div></a>
         </div>
-        <div class="day col-sm">
+        <div class="day">
             <div class="dayheader">Vendredi</div>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Fax 144  Transmission</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check    Ambulance et  Communication</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
-            <a href="?action=Edittodo" class="over" > <div class="hour">Check bibliothèque</div></a>
-            <a href="?action=Edittodo" class="over" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
-            <a href="?action=Edittodo" class="over" > <div class="hour">Nettoyage centrale et garage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Tâches spécifique</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Formation</div></a>
-            <a href="?action=Edittodo" class="over" > <div class="hour">Remise locaux ambulances</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Fax 144  Transmission</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check    Ambulance et  Communication</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check bibliothèque</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Nettoyage centrale et garage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Tâches spécifique</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Formation</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Remise locaux ambulances</div></a>
         </div>
-        <div class="day col-sm">
+        <div class="day">
             <div class="dayheader">Samedi</div>
-            <a href="?action=Edittodo"class="over" > <div class="hour">Fax 144  Transmission</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check    Ambulance et  Communication</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check bibliothèque</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Changer Bac chariot de nettoyage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Nettoyage centrale et garage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Tâches spécifique</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Formation</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Remise locaux ambulances</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Fax 144  Transmission</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check    Ambulance et  Communication</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check bibliothèque</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Nettoyage centrale et garage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Tâches spécifique</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Formation</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Remise locaux ambulances</div></a>
         </div>
-        <div class="day col-sm">
+        <div class="day">
             <div class="dayheader">Dimanche</div>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Fax 144  Transmission</div></a>
-            <a href="?action=Edittodo" class="over" > <div class="hour">Check    Ambulance et  Communication</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Check bibliothèque</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Changer Bac chariot de nettoyage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Nettoyage centrale et garage</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Tâches spécifique</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Formation</div></a>
-            <a href="?action=Edittodo" class="over"> <div class="hour">Remise locaux ambulances</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Fax 144  Transmission</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check    Ambulance et  Communication</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Contrôle stupéfiants  +  Date perf. Chaudes</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Check bibliothèque</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Changer Bac chariot de nettoyage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Nettoyage centrale et garage</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Tâches spécifique</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Formation</div></a>
+            <a href="?action=Edittodo" > <div class="hour">Remise locaux ambulances</div></a>
         </div>
 
-    <hr>
+
+
+<div class="week">
+    <div>
+        <div class="parent horizontal">jour</div>
+
+    </div>
     <div class="week">
         <div class="horizontal nuitcolor"> <span style="font-weight: bold">NUIT</span> </div>
         <div class="day">
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
 
+
+    <div class="dayheader">Lundi</div>
+    <div class="hour" >Do something</div>
+</div>
+<div class="day">
+    <div class="dayheader">Mardi</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+</div>
+<div class="day">
+    <div class="dayheader">Mercredi</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+</div>
+<div class="day">
+    <div class="dayheader">Jeudi</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+</div>
+<div class="day">
+    <div class="dayheader">Vendredi</div>
+    <div class="hour">Do something</div>
+</div>
+<div class="day">
+    <div class="dayheader">Samedi</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+</div>
+<div class="day">
+    <div class="dayheader">Dimanche</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+    <div class="hour">Do something</div>
+</div>
+</div>
+<div class="row m-2">
+
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
         </div>
         <div class="day">
 
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
         </div>
         <div class="day">
 
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
         </div>
         <div class="day">
 
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
         </div>
         <div class="day">
 
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
         </div>
         <div class="day">
 
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
         </div>
         <div class="day">
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
-            <a class="over"><div class="hour">Do something</div> </a>
+
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
+            <div class="hour">Do something</div>
         </div>
     </div>
 </div>
