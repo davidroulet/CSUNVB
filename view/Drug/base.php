c12fca94583067e36ce6ba7306f55044862e95fe
Fabien Masson <47849849+fabien-masson@users.noreply.github.com>
2020-03-02 14:09:44 +0100

    affichage des données en fonctinos de la base choisie

--- view/Drug/base.php
+++ view/Drug/base.php
@@ -9,11 +9,11 @@ $title = "CSU-NVB - Stupéfiants";
     <button class="btn">Tests</button>
 </form>
 <form action="/index.php?action=drugHomePage" method="post">
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value="Payerne">Payerne</button>
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value="Payerne">Yverdon</button>
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value="Payerne">Saint-Loup</button>
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value="Payerne">Sainte-Croix</button>
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value="Payerne">Vallée-de-Joux</button>
+    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=4>Payerne</button>
+    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=1>Yverdon</button>
+    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=3>Saint-Loup</button>
+    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=2>Sainte-Croix</button>
+    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=5>Vallée-de-Joux</button>
 </form>
 <?php
 $content = ob_get_clean();
--- view/Drug/drugHome.php
+++ view/Drug/drugHome.php
@@ -14,7 +14,13 @@ $year = 20;
 <form action="/index.php?action=drugSiteTable" method="post">
     <div class="row m-2">
         <?php
-        $weeks=getStupSheets();
+
+                $liste=getStupSheets();
+                foreach ($liste as $item){
+                    if($item["base_id"]==$_SESSION["Selectsite"]){
+                        $weeks[]=$item;
+                    }
+                }
         foreach ($weeks as $week) { ?>
             <button class="text-decoration-none card col-4 menutile align-items-center colorblue"
                     name="semaine" value="<?= $week["week"] ?>"> <?php echo $week["week"] ?> </button> <?php } ?>
