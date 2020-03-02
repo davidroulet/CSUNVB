690461623b53c01e475d6e614dfbec3c90983a96
ROULET David <David.ROULET@cpnv.ch>
2020-03-02 15:49:00 +0100

    Index Modfiy and teste

--- controler/drugControler.php
+++ controler/drugControler.php
@@ -11,49 +11,4 @@ function drugSiteTable($semaine,$Site){
 function drugbase(){
     require_once 'view/Drug/base.php';
 }
-function Teste(){
-    Echo "Teste----------------------------------------------------------------------------<br>";
-    echo "ALL Info?     ";
-    if (getStupSheets()!=null){
-        echo "OK<br>";
-    }else{
-        echo "No";
-    }
-    $data=readSheet(2);
-    echo "PRECI Info?     ";
-    if ($data["week"]==2009){
-        echo "OK<br>";
-    }else{
-        echo "No";
-    }
-    echo "Update Info?     ";
-    $data["state"]="open";
-    updateSheet($data);
-    $data2=readSheet("2");
-    if ($data2["state"]=="open"){
-        echo "OK<br>";
-    }else{
-        echo "No";
-    }
-    $data["state"]="closed";
-    updateSheet($data);
-    echo "New Info?     ";
-    $dataC=["week"=>"3002","state"=> "open","base_id"=>3];
-    $idit=createSheet($dataC);
-    $data3=readSheet($idit["id"]);
-    if ($data3["week"]=="3002"){
-        echo "OK<br>";
-    }else{
-        echo "No";
-    }
-    echo "DEL Info?     ";
-    $delitem=readSheet($idit["id"]);
-    destroySheet($delitem["id"]);
-    if (readSheet($idit["id"])==null){
-        echo "OK<br>";
-    }else{
-        echo "No";
-    }
-
-}
 ?>
