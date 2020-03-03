d4a75021e7332ddf84fa3b9d8ad88fed58d22f1e
ALHELO Marwan <Marwan.ALHELO@cpnv.ch>
2020-03-03 16:38:58 +0100

    add edittodo + weeklist

--- view/todo/edittodopage.php
+++ view/todo/edittodopage.php
@@ -10,8 +10,206 @@ $title = "CSU-NVB - Tâches hebdomadaires";
 ?>
 
 
+<h1 class="center"><?= $title ?></h1>
+<div class="container ">
+    <div class="week">
+        <div class="horizontal"><span style="font-weight: bold"> Journée</span></div>
+        <div class="day col-sm">
+            <div class="dayheader">Lundi</div>
+            <div class="weekhead" > semaine N°</div>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+        </div>
+        <div class="day col-sm">
+            <div class="dayheader">Mardi</div>
+            <div class="weekhead" > semaine N°</div>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+        </div>
+        <div class="day col-sm">
+            <div class="dayheader">Mercredi</div>
+            <div class="weekhead" > semaine N°</div>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+        </div>
+        <div class="day col-sm">
+            <div class="dayheader">Jeudi</div>
+            <div class="weekhead" > semaine N°</div>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+        </div>
+        <div class="day col-sm">
+            <div class="dayheader">Vendredi</div>
+            <div class="weekhead" > semaine N°</div>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+        </div>
+        <div class="day col-sm">
+            <div class="dayheader">Samedi</div>
+            <div class="weekhead" > semaine N°</div>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+        </div>
+        <div class="day col-sm">
+            <div class="dayheader">Dimanche</div>
+            <div class="weekhead" > semaine N°</div>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+            <a href="?action=Edittodo" class="over"><div class="hour">Fax 144 Transmission </div></a>
+        </div>
+
+        <div class="week">
+            <div class="horizontal nuitcolor"><span style="font-weight: bold">NUIT</span></div>
+            <div class="day col-sm">
+                <a href="" class="over">
+                <div class="hour">Contrôle stupéfiants Ambulances
+                        * Morphine X4
+                        * Sintenyl X6
+                </div>
+                </a>
+                <div class="hour">
+                    <input type="text" class="form-control" placeholder=" Nova N°....................">
+                </div>
+                <a href="" class="over"><div class="hour"> Tâches spécifique </div></a>
+                <a href="" class="over"><div class="hour"> Remise locaux Transmission </div></a>
+
+            </div>
+            <div class="day col-sm">
+                <a href="" class="over">
+                    <div class="hour">Contrôle stupéfiants Ambulances
+                        * Morphine X4
+                        * Sintenyl X6
+                    </div>
+                </a>
+                <div class="hour">
+                    <input type="text" class="form-control" placeholder=" Nova N°....................">
+                </div>
+                <a href="" class="over"><div class="hour"> Tâches spécifique </div></a>
+                <a href="" class="over"><div class="hour"> Remise locaux Transmission </div></a>
+
+            </div>
+            <div class="day col-sm">
+                <a href="" class="over">
+                    <div class="hour">Contrôle stupéfiants Ambulances
+                        * Morphine X4
+                        * Sintenyl X6
+                    </div>
+                </a>
+                <div class="hour">
+                    <input type="text" class="form-control" placeholder=" Nova N°....................">
+                </div>
+                <a href="" class="over"><div class="hour"> Tâches spécifique </div></a>
+                <a href="" class="over"><div class="hour"> Remise locaux Transmission </div></a>
+
+            </div>
+            <div class="day col-sm">
+                <a href="" class="over">
+                    <div class="hour">Contrôle stupéfiants Ambulances
+                        * Morphine X4
+                        * Sintenyl X6
+                    </div>
+                </a>
+                <div class="hour">
+                    <input type="text" class="form-control" placeholder=" Nova N°....................">
+                </div>
+                <a href="" class="over"><div class="hour"> Tâches spécifique </div></a>
+                <a href="" class="over"><div class="hour"> Remise locaux Transmission </div></a>
+
+            </div>
+            <div class="day col-sm">
+                <a href="" class="over">
+                    <div class="hour">Contrôle stupéfiants Ambulances
+                        * Morphine X4
+                        * Sintenyl X6
+                    </div>
+                </a>
+                <div class="hour">
+                    <input type="text" class="form-control" placeholder=" Nova N°....................">
+                </div>
+                <a href="" class="over"><div class="hour"> Tâches spécifique </div></a>
+                <a href="" class="over"><div class="hour"> Remise locaux Transmission </div></a>
+
+            </div>
+            <div class="day col-sm">
+                <a href="" class="over">
+                    <div class="hour">Contrôle stupéfiants Ambulances
+                        * Morphine X4
+                        * Sintenyl X6
+                    </div>
+                </a>
+                <div class="hour">
+                    <input type="text" class="form-control" placeholder=" Nova N°....................">
+                </div>
+                <a href="" class="over"><div class="hour"> Tâches spécifique </div></a>
+                <a href="" class="over"><div class="hour"> Remise locaux Transmission </div></a>
+
+            </div>
+            <div class="day col-sm">
+                <a href="" class="over">
+                    <div class="hour">Contrôle stupéfiants Ambulances
+                        * Morphine X4
+                        * Sintenyl X6
+                    </div>
+                </a>
+                <div class="hour">
+                    <input type="text" class="form-control" placeholder=" Nova N°....................">
+                </div>
+                <a href="" class="over"><div class="hour"> Tâches spécifique </div></a>
+                <a href="" class="over"><div class="hour"> Remise locaux Transmission </div></a>
+
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 $content = ob_get_clean();
 require "view/gabarit.php";
 ?>
-
