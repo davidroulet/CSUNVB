4f2b9943f9d42c93f2ccf3bbc5b7ef4521448203
Mounir37 <47849542+Mounir37@users.noreply.github.com>
2020-03-05 12:08:44 +0100

    commencement des vérification des cases

--- view/viewsShiftEnd/shiftEndHome.php
+++ view/viewsShiftEnd/shiftEndHome.php
@@ -32,35 +32,35 @@ $title = "CSU-NVB - Remise de garde";
                 <td id="td_Rad_J" onclick="fn_Chk_Rad_J()"><input type="checkbox" id="chk_Rad_J" style="visibility: hidden"></td>
                 <td id="td_Rad_N" onclick="fn_Chk_Rad_N()"><input type="checkbox" id="chk_Rad_N" style="visibility: hidden"></td>
                 <td><textarea style="resize: none" id="TXT_Area_Rad" cols="100%" rows="3"></textarea></td>
-                <td id="td_Rad_Val"><span id="" class="glyphicon glyphicon-ok">X</span></td>
+                <td id="td_Rad_Val"><span id="span_Rad_Val_Ok" class="glyphicon glyphicon-ok">O</span></td>
             </tr>
             <tr>
                 <td><div id="lbl_Det_CO">Détecteurs CO</div></td>
                 <td id="td_Det_CO_J" onclick="fn_Chk_Det_CO_J()"><input type="checkbox" id="chk_Det_CO_J" style="visibility: hidden"></td>
                 <td id="td_Det_CO_N" onclick="fn_Chk_Det_CO_N()"><input type="checkbox" id="chk_Det_CO_N" style="visibility: hidden"></td>
                 <td><textarea style="resize: none" id="TXT_Area_Det_Co" cols="100%" rows="3"></textarea></td>
-                <td id="td_Det_CO_Val"></td>
+                <td id="td_Det_CO_Val"><span id="span_Det_CO_Val_Ok" class="glyphicon glyphicon-ok">O</span></td>
             </tr>
             <tr>
                 <td><div id="lbl_Tel">Téléphones</div></td>
                 <td id="td_Tel_J" onclick="fn_Chk_Tel_J()"><input type="checkbox" id="chk_Tel_J" style="visibility: hidden"></td>
                 <td id="td_Tel_N" onclick="fn_Chk_Tel_N()"><input type="checkbox" id="chk_Tel_N" style="visibility: hidden"></td>
                 <td><textarea style="resize: none" id="TXT_Area_Tel" cols="100%" rows="3"></textarea></td>
-                <td id="td_Tel_Val"></td>
+                <td id="td_Tel_Val"><span id="span_Tel_Val_Ok" class="glyphicon glyphicon-ok">O</span></td>
             </tr>
             <tr>
                 <td><div id="lbl_GT">Gt info avisé</div></td>
                 <td id="td_GT_J" onclick="fn_Chk_GT_J()"><input type="checkbox" id="chk_GT_J" style="visibility: hidden"></td>
                 <td id="td_GT_N" onclick="fn_Chk_GT_N()"><input type="checkbox" id="chk_GT_N" style="visibility: hidden"></td>
                 <td><textarea style="resize: none" id="TXT_Area_GT" cols="100%" rows="3"></textarea></td>
-                <td id="td_GT_Val"></td>
+                <td id="td_GT_Val"><span id="span_Rad_GT_Ok" class="glyphicon glyphicon-ok">O</span></td>
             </tr>
             <tr>
                 <td><div id="lbl_Ann">annonce 144</div></td>
                 <td id="td_Ann_J" onclick="fn_Chk_Ann_J()"><input type="checkbox" id="chk_Ann_J" style="visibility: hidden"></td>
                 <td id="td_Ann_N" onclick="fn_Chk_Ann_N()"><input type="checkbox" id="chk_Ann_N" style="visibility: hidden"></td>
                 <td><textarea style="resize: none" id="TXT_Area_Ann" cols="100%" rows="3"></textarea></td>
-                <td id="td_Rad_Val"></td>
+                <td id="td_Rad_Val"><span id="span_Ann_Val_Ok" class="glyphicon glyphicon-ok">O</span><span id="span_Ann_Val_NO" class="glyphicon glyphicon-ok">X</span></td>
             </tr>
 
             </tbody>
