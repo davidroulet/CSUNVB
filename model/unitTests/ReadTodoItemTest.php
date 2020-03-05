f0f61ca10f9e4b493aeaa5bcbef1e0707330822f
AltinZili <47849648+AltinZili@users.noreply.github.com>
2020-03-05 12:09:18 +0100

    Read ToDo Items presque fini

--- model/unitTests/ReadTodoItemTest.php
+++ model/unitTests/ReadTodoItemTest.php
@@ -24,31 +24,44 @@ $todoItems = readTodoListItems();
 }*/
 
 
-
 echo "Test unitaire de la fonction readTodoListItem (by Id)\n\n";
 
-echo "Test 1ère partie - tester que les bons champs soient retournés";
+echo "Test 1ère partie - tester que les bons champs soient retournés\n\n";
 
 $item = readTodoListItem(2);
 
-if($item['id'] == 2 && $item['week'] == 2009 && $item['state'] == "closed" && $item['base_id'] == 1){
-    echo "Test réussi";
+if ($item['id'] == 2 && $item['week'] == 2009 && $item['state'] == "closed" && $item['base_id'] == 1) {
+    echo "-> Test réussi\n\n";
 } else {
-    echo "Test échoué";
+    echo "-> Test échoué\n\n";
 }
 
-echo "Test 2ème partie - tester que le nombre de champ soit juste \n\n";
+echo "Test 2ème partie - tester que le nombre de champs soit juste \n\n";
 
+$items = readTodoListItems();
+$countFieldsItems = 0;
 
+foreach ($items as $item) {
+    $number = count($item);
+    if ($number == 4) {
+        $countFieldsItems += 1;
+    }
+}
+
+if ($countFieldsItems == 22) {
+    echo "-> Test réussi\n\n";
+} else {
+    echo "-> Test échoué\n\n";
+}
 
 echo "Test 3ème partie - tester que si on met un autre id, ça ne retourne pas les mêmes champs\n\n";
 
 $item2 = readTodoListItem(3);
 
-if($item2['id'] == 2 && $item2['week'] == 2009 && $item2['state'] == "closed" && $item2['base_id'] == 1){
-    echo "Test échoué";
+if ($item2['id'] == 2 && $item2['week'] == 2009 && $item2['state'] == "closed" && $item2['base_id'] == 1) {
+    echo "-> Test échoué\n\n";
 } else {
-    echo "Test réussi";
+    echo "-> Test réussi\n\n";
 }
 
 ?>
