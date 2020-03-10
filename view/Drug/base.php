a9f88258706c8af7112c76de2bb70086aa86abfa
Fabien Masson <47849849+fabien-masson@users.noreply.github.com>
2020-03-10 16:47:00 +0100

    wip

--- view/Drug/base.php
+++ view/Drug/base.php
@@ -12,11 +12,11 @@ $title = "CSU-NVB - Stupéfiants";
     <button class="btn">Tests</button>
 </form>
 <form action="/index.php?action=drugHomePage" method="post">
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=4>Payerne</button>
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=1>Yverdon</button>
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=3>Saint-Loup</button>
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=2>Sainte-Croix</button>
-    <button class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=5>Vallée-de-Joux</button>
+    <button class="text-decoration-none card col-6 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=4>Payerne</button>
+    <button class="text-decoration-none card col-6 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=1>Yverdon</button>
+    <button class="text-decoration-none card col-6 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=3>Saint-Loup</button>
+    <button class="text-decoration-none card col-6 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=2>Sainte-Croix</button>
+    <button class="text-decoration-none card col-6 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=5>Vallée-de-Joux</button>
 </form>
 <?php
 $content = ob_get_clean();
