42c7711286b9255098c40106adc51a7063d41ab7
ROULET David <David.ROULET@cpnv.ch>
2020-03-12 10:43:59 +0100

    Remouve thing

--- view/Drug/base.php
+++ view/Drug/base.php
@@ -8,9 +8,7 @@ $title = "CSU-NVB - Stupéfiants";
 <div class="row m-2">
     <h1>Stupéfiants</h1>
 </div>
-<form action="/index.php?action=DrugTest" method="post">
-    <button class="btn">Tests</button>
-</form>
+
 <form action="/index.php?action=drugHomePage" method="post">
     <button class="text-decoration-none card col-6 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=4>Payerne</button>
     <button class="text-decoration-none card col-6 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" name="site" value=1>Yverdon</button>
