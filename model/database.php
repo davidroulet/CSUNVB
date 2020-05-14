798748325c4753a96f86027244595893a19e243e
MisterX25 <Xavier.Carrel@cpnv.ch>
2020-05-14 09:08:42 +0200

    Ajouté database.php pour les fonctions d'accès db

---
+++ model/database.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * File : database.php
+ * Author : X. Carrel
+ * Created : 2020-05-13
+ * Modified last :
+ **/
+
+function getPDO()
+{
+    require ".const.php";
+    $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
+    return $dbh;
+}
+
+function select($query, $params, $multirecord)
+{
+    require ".const.php";
+    $dbh = getPDO();
+    try
+    {
+        $statement = $dbh->prepare($query);//prepare query
+        $statement->execute($params);//execute query
+        if ($multirecord)
+        {
+            $queryResult = $statement->fetchAll(PDO::FETCH_ASSOC);
+        } else
+        {
+            $queryResult = $statement->fetch(PDO::FETCH_ASSOC);
+        }
+        $dbh = null;
+        return $queryResult;
+    } catch (PDOException $e)
+    {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
+}
+
+function selectMany($query, $params)
+{
+    return select($query, $params, true);
+}
+
+
+
+?>
