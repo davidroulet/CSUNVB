fe2b4703f4bb1aeaaf492f1dc7ceef840d31402f
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2020-06-04 11:27:00 +0200

    Update drugModel.php

--- model/drugModel.php
+++ model/drugModel.php
@@ -374,6 +374,20 @@ function destroyNova($id)
  */
 function getDrugs()
 {
+    require ".const.php";
+    $dbh = getPDO();
+    try {
+        $query = 'SELECT * FROM drugs';
+        $statement = $dbh->prepare($query);//prepare query
+        $statement->execute();//execute query
+        $queryResult = $statement->fetchAll(PDO::FETCH_ASSOC);//prepare result for client
+        $dbh = null;
+        if ($debug) var_dump($queryResult);
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
     // TODO Coder cette fonction avec PDO
 }
 
