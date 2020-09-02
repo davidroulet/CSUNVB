d6105a0d60baf16d31929adff34b1ebd15875ec9
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2020-09-02 15:05:00 +0200

    essaie d afficher date et etat pour shiftend

--- model/guardSheetModel.php
+++ model/guardSheetModel.php
@@ -105,5 +105,8 @@ function getGuardSheetsByBase($base_id)
 {
     return selectOne('select * from guardsheets where base_id=:base_id', ['base_id' => $base_id]);
 }
+function getGuradSheetWeek($week,$base){
+    return selectOne('SELECT * FROM guardsheets INNER JOIN bases ON bases.id=base_id WHERE week =:week AND base_id=:base', ['week' => $week, 'base' => $base]);
+}
 
 ?>
