9c5a60f4d1965f177c22fe56762261f5f0e63758
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2020-09-09 15:01:01 +0200

    Update shiftEndControler.php

--- controler/shiftEndControler.php
+++ controler/shiftEndControler.php
@@ -24,10 +24,4 @@ function listShiftEnd($base_id)
 
     require_once 'view/viewsShiftEnd/ListShiftEnd.php';
 }
-function  ShiftEndTable($semaine){
-
-    $jourDebutSemaine = getdate2($semaine); // récupère les jours de la semaine en fonction de la date sélectio
-    $GuardSheet = getGuradSheetWeek($semaine, $_SESSION["Selectsite"]); // la feuille de stupéfiants à afficher
-    require_once 'view/viewsShiftEnd/ListShiftEnd.php';
-}
 ?>
