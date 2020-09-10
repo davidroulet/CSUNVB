bfe18b21f98437cddbd9f888e1fb461691af357a
Mounir37 <47849542+Mounir37@users.noreply.github.com>
2020-09-10 08:16:11 +0200

    function addNewDrugs

--- model/adminModel.php
+++ model/adminModel.php
@@ -99,9 +99,9 @@ function SaveBase($bases)       //Met à jour les informations d'une base
     return execute("UPDATE bases SET name= :name where id = :bases", [$bases]);
 }
 
-function addNewDrug($nameDrug)
+function addNewDrug($id, $nameDrug)
 {
-    return insert("INSERT INTO Drugs values (:id, :nameDrugs) ",['name'=>$nameDrug] );
+    return insert("INSERT INTO Drugs values (:id, :nameDrugs) ",['id'=>$id, 'name'=>$nameDrug] );
 }
 
 function addNewUser($id, $prenomUser, $nomUser, $initialesUser, $hash, $admin, $firstconnect)
