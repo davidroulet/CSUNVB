d6b69527c273e3b915003223f7d2c3964f0c7541
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2020-09-25 14:10:38 +0200

    Update adminHome.php

--- view/Admin/adminHome.php
+++ view/Admin/adminHome.php
@@ -2,7 +2,7 @@
 ob_start();
 $title = "CSU-NVB - Administration";
 ?>
-<div class="row m-2">
+<div class="row m-4 d-flex justify-content-center">
     <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=adminCrew">Secouristes</a>
     <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=adminBases">Bases</a>
     <a class="text-decoration-none card col-4 menutile pl-3 pr-3 pt-5 pb-5 m-1 align-items-center" href="?action=adminNovas">Ambulances</a>
